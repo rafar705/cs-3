5291e0b6fbb62b81d9d311476479365e50fb85d1
rafar705 <rafatali772673090@gmail.com>
2025-03-03 13:37:35 +0300

    Assignment Four
    Assignment Four2بعد التعديل

---
+++ Assignment Four 2/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super .key });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Counter App')),
+      body: Center(
+        child: Text(
+          '$_counter',
+          style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: buildRow(),
+      ),
+    );
+  }
+
+  Row buildRow() {
+    return buildRow1();
+  }
+
+  Row buildRow1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Decrement',
+          backgroundColor: Colors.red,
+          child: Icon(Icons.remove),
+        ),
+        SizedBox(width: 16),
+        FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+}
